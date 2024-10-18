5dcc90647d4440ead965f9de1eb8fdc03baa311e
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-10-18 16:23:22 +0200

    f

--- src/pg_gen/game_core/ResourceProvider.py
+++ src/pg_gen/game_core/ResourceProvider.py
@@ -14,7 +14,7 @@ class ResourceProvider:
         )
 
         self.key_sprite = spritesheet.slice(Point(32, 0), Point(16, 16))
-        self.door_sprite = spritesheet.slice(Point(0, 0), Point(32, 48))
+        self.door_sprite = spritesheet.slice(Point(0, 0), Point(32, 32))
         self.wall_sprite = spritesheet.slice(Point(32, 16), Point(16, 16))
         self.ladder_sprite = spritesheet.slice(Point(48, 16), Point(16, 16))
         self.pole_sprite = spritesheet.slice(Point(48, 32), Point(16, 16))
