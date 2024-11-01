f8de90ebf94a71a81a5e0e010671f0cfb1e11454
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-11-01 16:28:40 +0100

    Persistence for gems

--- src/pg_gen/actors/Gem.py
+++ src/pg_gen/actors/Gem.py
@@ -1,22 +1,32 @@
 from dataclasses import dataclass, field
+from typing import Any
 
 from pg_gen.world.Actor import Actor
 
 from ..level_editor.ActorRegistry import ActorRegistry
 from ..world.CollisionFlags import CollisionFlags
 from .Player import Player
+from .support.PersistentObject import PersistentObject
 from .support.SpriteActor import SpriteActor
 
 
 @dataclass
-class Gem(SpriteActor):
+class Gem(SpriteActor, PersistentObject[bool]):
     sprite: str | None = field(default="gem_sprite")
     collision_flags: CollisionFlags = CollisionFlags.TRIGGER
+
+    def _get_default_persistent_value(self) -> Any:
+        return True
+
+    def on_created(self):
+        if self.persistent_value == False:
+            self.universe.queue_task(lambda: self.remove())
 
     def on_trigger(self, trigger: Actor):
         if isinstance(trigger, Player):
             trigger.score += 10
             self.universe.queue_task(lambda: self.remove())
+            self.persistent_value = False
         return super().on_trigger(trigger)
 
 
