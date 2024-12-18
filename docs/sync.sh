78dbc2e49da54ba30d76573365c759b70b855c6f
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-12-18 14:10:45 +0100

    [Docs] Fixed exporting of nested numbered lists

--- docs/sync.sh
+++ docs/sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 wget -O doc.html https://docs.google.com/document/d/1lg1TWKU5NMvVqQq4Px7R0y2xmIzQqSgwR5Gl5SJ39FY/mobilebasic?tab=t.rotwa4lwlugz
-ucpem run @/MiniML+cli build doc.html doc.tex --htmlSelector=.doc-content --htmlCite
+ucpem run @/MiniML+cli build doc.html doc.tex --htmlSelector=.doc-content --htmlCite --htmlNormalizeLists
